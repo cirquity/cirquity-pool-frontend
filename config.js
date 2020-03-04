0e72c78b8b2e53fd0e01b4f55ede415c84e4ccec
Deeterd <deeterd@pm.me>
2020-03-04 15:10:29 +0100

    Redesign pool frontend

---
+++ config.js
@@ -0,0 +1,23 @@
+let api = "https://api-pool.cirquity.com";
+let parentCoin = "cirquity";
+let email = "cirquity@pm.me";
+let telegram = "https://t.me/cirquity";
+let discord = "https://chat.cirquity.com";
+let facebook = "https://www.facebook.com/cirquity";
+let marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+let blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+let blockchainExplorerMerged = "http://explorer.ird.cash/?hash={id}#block";
+let transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+let transactionExplorerMerged = "http://explorer.ird.cash/?hash={id}#transaction";
+let themeCss = "themes/default.css";
+let defaultLang = 'en';
+let languages = {
+    'en': 'English',
+    'es': 'Español',
+    'fr': 'Français',
+    'it': 'Italiano',
+    'ru': 'Русский',
+    'ca': 'Català',
+    'ko': '한국어',
+    'zh-CN': '简体中文'
+};
